1daac4b2cd13b3789e25dd370025c9c8bf6fd351
Tasmiya651 <68432307+Tasmiya651@users.noreply.github.com>
2020-07-18 16:20:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var balloon=$(".balloon")
 var counter=0
 var colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 //number of ballons on screen
-// var balloonNumber=Math.random()*10
-balloonNumber = 9
+var balloonNumber=Math.random()*16
+
 
 for(i=0;i<balloonNumber;i++){
   var balloon_color
